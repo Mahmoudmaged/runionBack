1e1076c38a84e4d7e2f6fd7568ce99296c1ee771
Mahmoud <mahmoudelwan460@gmail.com>
2021-06-06 18:18:50 +0200

    addk

--- app.js
+++ app.js
@@ -5,47 +5,46 @@ const path = require("path")
 const multer = require("multer");
 var cors = require('cors')
 const mongoose = require('mongoose');
-app.use(express.urlencoded({extends:false}));
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.urlencoded({ extends: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use(express.static(path.join("uploadImages")))
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.get('/', (req, res) => {
-   res.json({message:"welcome  In Reunion System"}) ;
+    res.json({ message: "welcome  In Reunion System" });
 });
 app.use("/uploadImages", express.static(path.join("uploadImages")))
 
 app.use(cors());
 var storage = multer.diskStorage({
-    destination:(req,file,cb)=>{
-        cb(null , "uploadImages");
+    destination: (req, file, cb) => {
+        cb(null, "uploadImages");
     },
-    filename:(req,file,cb)=>{
-        cb(null,file.originalname)
+    filename: (req, file, cb) => {
+        cb(null, file.originalname)
     }
 
-}) 
-function fileFilter (req, file, cb ) {
+})
+function fileFilter(req, file, cb) {
 
 
-    if(file.mimetype==="image/png" 
-       ||file.mimetype==="image/jpg"
-       ||file.mimetype==="image/jpeg")
-    {
+    if (file.mimetype === "image/png"
+        || file.mimetype === "image/jpg"
+        || file.mimetype === "image/jpeg") {
         cb(null, true)
 
-    }else 
-    {
+    } else {
         cb("invalid file type only png , jpg and jpeg is acceptable", false)
 
     }
-  
+
 }
-app.use(multer({dest:"files" ,fileFilter, storage }).single("img"));
+app.use(multer({ dest: "files", fileFilter, storage }).single("img"));
 app.use(require("./routers/app.router"));
 
-mongoose.connect('mongodb+srv://MahmoudElwan:01015776658@mahmoudelwan-nodejs.jfspq.mongodb.net/ReunionGP', {useNewUrlParser: true, useUnifiedTopology: true}).then(()=>{
-    console.log("mongoDB is Connected");
-});
+mongoose.connect('mongodb+srv://MahmoudElwan:01015776658@mahmoudelwan-nodejs.jfspq.mongodb.net/ReunionGP',
+    { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+        console.log("mongoDB is Connected");
+    });
 app.listen(port, () => console.log(`Example app listening on port port!`))
