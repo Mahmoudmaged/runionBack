b6e15d229a52357c327dfba5647f72d127da621c
Mahmoud <mahmoudelwan460@gmail.com>
2021-06-11 17:49:18 +0200

    policeStation not policeStations

--- controller/superAdmin/displayPoliceSationSignUpRequest.controller.js
+++ controller/superAdmin/displayPoliceSationSignUpRequest.controller.js
@@ -1,7 +1,7 @@
 const userModel = require("../../model/user.model")
 module.exports = async (req, res) => {
     try {
-        const requestList = await userModel.find({ aprove: false, confirmEmail: true  , role:"policeSations"});
+        const requestList = await userModel.find({ aprove: false, confirmEmail: true  , role:"policeStation"});
         res.json({message:'done',requestList});
     } catch (error) {
         res.json({ message: "can not get any users" });
