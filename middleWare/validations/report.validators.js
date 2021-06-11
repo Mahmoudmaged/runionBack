2bf666d82eb167b8eb6d980f810c64306d24c641
Mahmoud <mahmoudelwan460@gmail.com>
2021-06-11 22:04:33 +0200

    isString

--- middleWare/validations/report.validators.js
+++ middleWare/validations/report.validators.js
@@ -5,7 +5,7 @@ module.exports =[
     body("age").isNumeric(),
     body("gender").isString(),
     body("description").isString(),
-    body("lostLocation").contains('https://www.google.com/maps'),
+    body("lostLocation").isString(),
     body("lostTime").isDate(),
     body("reporterName").matches(/^[\u0600ء-ي]+[\u0600ء-ي-_\.][^#&<>\"~;$^%{}?]{1,20}$/),
     body("reporterNationID").matches(/(2|3)[0-9][1-9][0-1][1-9][0-3][1-9](01|02|03|04|11|12|13|14|15|16|17|18|19|21|22|23|24|25|26|27|28|29|31|32|33|34|35|88)\d\d\d\d\d/),
