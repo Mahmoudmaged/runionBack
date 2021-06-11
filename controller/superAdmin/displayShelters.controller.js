f0ffa88c1a1968e21378cdcd221fbce69cc3d424
Mahmoud <mahmoudelwan460@gmail.com>
2021-06-11 20:12:07 +0200

    aproveShelter

--- controller/superAdmin/displayShelters.controller.js
+++ controller/superAdmin/displayShelters.controller.js
@@ -3,7 +3,7 @@ const userModel = require("../../model/user.model")
 
 module.exports = async(req,res)=>{
     try {
-        const shelterList = await userModel.find({role:"shelter"});
+        const shelterList = await userModel.find({role:"shelter" , aprove:true});
        res.json({message:"done" , shelterList}) 
 
     } catch (error) {
